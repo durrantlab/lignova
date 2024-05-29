5603da8f4ce67476616ff4160dcf0df1a54735ce
Mayarmohamed <mma121@pitt.edu>
2024-05-29 15:14:26 -0400

    finished tanimoto cluster function

--- lignova/clustering/tanimoto.py
+++ lignova/clustering/tanimoto.py
@@ -14,7 +14,7 @@ from rdkit.ML.Cluster import Butina
 class TanimotoClustering:
     r"""Tanimoto clustering algorithm."""
 
-    def __init__(self, similarity_threshold: float):
+    def __init__(self, similarity_threshold: float = 0.2):
         r"""Initialize the Tanimoto clustering algorithm.
         parameters:
         ----------
@@ -63,35 +63,56 @@ class TanimotoClustering:
         else:
             return DataStructs.TanimotoSimilarity(mol1, mol2)
 
-    def cluster_tanimoto(self, tanimoto_score: Dict[str, float]) -> List[List[str]]:
-        r"""Cluster the SMILES based on Tanimoto similarity.
+    def cal_distance(
+        self, tanimoto_score: list[list[float]] | list[float]
+    ) -> List[float]:
+        r"""Calculate the distance between two fingerprints.
+        parameters:
+        ----------
+            tanimoto_score: list[list[float]]
+                List of Tanimoto similarity scores.
+        returns:
+        ----------
+            float: Distance.
+        """
+        # knowing that the tanimoto_score is a list of list of similarity scores
+        # we will calculate the distance between the fingerprints using the formula 1 - score
+        distance = []
+        if isinstance(tanimoto_score[0], float):
+            for i in range(len(tanimoto_score)):
+                distance.append(1 - tanimoto_score[i])
+            return distance
+        for i in range(len(tanimoto_score)):
+            tmp = []
+            for j in range(len(tanimoto_score[i])):
+                tmp.append(1 - tanimoto_score[i][j])
+            distance.extend(tmp)
+        logger.debug(f"Distances: {distance}")
+        return distance
+
+    def cluster_tanimoto(
+        self, tanimoto_score: list[list[float]], smiles: list[str]
+    ) -> List[List[str]]:
+        r"""Cluster the SMILES based on Tanimoto similarity using butina algorithm.
         parameters:
         ----------
             tanimoto_score: Dict[str, float]
                 Dictionary of SMILES or compound identifiers and their Tanimoto similarity scores.
+            smiles: List[str]
+                List of SMILES or compound identifiers.
         returns:
         ----------
             List[List[str]]: Clustered .
         """
         clusters = []
-        for key, value in tanimoto_score.items():
-            if not clusters:
-                clusters.append([key])
-            else:
-                found = False
-                for cluster in clusters:
-                    for member in cluster:
-                        if (
-                            self.tanimoto_similarity(
-                                self.get_morgan_fingerprint(key),
-                                self.get_morgan_fingerprint(member),
-                            )
-                            > self.similarity_threshold
-                        ):
-                            cluster.append(key)
-                            found = True
-                            break
-                    if found:
-                        break
-                if not found:
-                    clusters.append([key])
+        # calculate the distance between the fingerprints using cal_distance
+        distance = self.cal_distance(tanimoto_score)
+        clusters = Butina.ClusterData(
+            distance, len(smiles), self.similarity_threshold, isDistData=True
+        )
+        logger.info(f"Number of clusters: {len(clusters)}")
+        logger.debug(f"Clusters: {clusters}")
+        # convert the clusters to smiles
+        clusters = [[smiles[i] for i in cluster] for cluster in clusters]
+        logger.info(f"Clusters: {clusters}")
+        return clusters
--- tests/test_tanimoto.py
+++ tests/test_tanimoto.py
@@ -66,5 +66,51 @@ def test_tanimoto_similarity_multiple_mols():
     assert similarity[2] == 0.09433962264150944
 
 
+def test_cal_distance():
+    smiles1 = [
+        "CC(=O)N1CCC(CC1)C2=NC3=CC=CC=C3N=C2OC4CN(C4)C5=NC6=CC=CC=C6C=C5",
+        "CC[C@@H](C(=O)N[C@@H](C1CCCCC1)C(=O)N2C[C@H]3CCCN3C[C@H]2C(=O)N[C@@H]4CCOC5=CC=CC=C45)NC",
+        "CC(=O)O",
+    ]
+    smiles2 = "CC(=O)N1CCC(CC1)C2=NC3=CC=CC=C3N=C2OC4CN(C4)C5=NC6=CC=CC=C6C=C5"
+    fingerprint1 = [
+        TanimotoClustering.get_morgan_fingerprint(smile) for smile in smiles1
+    ]
+    fingerprint2 = TanimotoClustering.get_morgan_fingerprint(smiles2)
+    similarity = TanimotoClustering.tanimoto_similarity(fingerprint1, fingerprint2)
+    logger.info(similarity)
+    distance = TanimotoClustering.cal_distance(similarity)
+    logger.info(distance)
+    assert distance[0] == 0.0
+    assert distance[1] == 0.8235294117647058
+    assert distance[2] == 0.9056603773584906
+
+
 def test_cluster_tanimoto():
-    pass
+    smiles = [
+        "COC1=CC=C(C=C1)CNC(=O)CCCNS(=O)(=O)C2=CC=CC3=NON=C32",
+        "CC(=O)N1CCC(CC1)C2=NC3=CC=CC=C3N=C2OC4CN(C4)C5=NC6=CC=CC=C6C=C5",
+        "CC(C)(C)OC(=O)N1CCC(CC1)C2=NC3=CC=CC=C3N=C2OC4CN(C4)C5=NC6=CC=CC=C6C=C5",
+        "C1CN(CCC1C2=NC3=CC=CC=C3N=C2OC4CN(C4)C5=NC6=CC=CC=C6C=C5)C(=O)O",
+    ]
+    # Get fingerprints for each molecule and tanimoto similarity
+    fingerprints = [
+        TanimotoClustering.get_morgan_fingerprint(smile) for smile in smiles
+    ]
+    similarity = []
+    # loop over the fingerprints to get the similarity for each molecule with the previous molecules
+    for i in range(1, len(fingerprints)):
+        similarity.append(
+            TanimotoClustering.tanimoto_similarity(fingerprints[:i], fingerprints[i])
+        )
+    logger.info(f"Similarity {similarity}")
+
+    # Cluster the molecules using cluster_tanimoto
+    clusters = TanimotoClustering.cluster_tanimoto(similarity, smiles)
+    assert len(clusters) == 2
+    assert len(clusters[0]) == 3
+    assert len(clusters[1]) == 1
+    assert clusters[0][0] == smiles[3]
+    assert clusters[0][1] == smiles[1]
+    assert clusters[0][2] == smiles[2]
+    assert clusters[1][0] == smiles[0]
