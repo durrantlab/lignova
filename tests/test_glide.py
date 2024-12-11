a5f4a679a5d65817b2cf731a6f958b2c94bfa242
Mayarmohamed <mma121@pitt.edu>
2024-12-11 07:52:23 -0500

    update it to fix pylint issues

--- tests/test_glide.py
+++ tests/test_glide.py
@@ -1,19 +1,20 @@
+"""
+This module contains tests for the Glide docking process.
+"""
+
 import csv
-import glob
 import os
-import shutil
 
 import numpy as np
-import pytest
+from loguru import logger
 
 from lignova.docking import Glide
 from lignova.docking.contexts import GlideContext
-from lignova.docking.docking import Docking
-from lignova.io import *
-from lignova.structure.editing import *
-from lignova.structure.ligand import Ligand, PreparedLigand
-from lignova.structure.protein import PreparedProtein, Protein
+from lignova.structure.editing import write_mda_universe
+from lignova.structure.ligand import Ligand
+from lignova.structure.protein import Protein
 from lignova.structure.utils import separate_protein_ligand
+
 
 # Ensures we execute from file directory (for relative paths).
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
@@ -32,6 +33,7 @@ context_protein_6Oav = {
 
 
 def prep_dirs():
+    """Prepare directories for writing files."""
     os.makedirs(context_protein_6Oav["write_dir"])
 
 
@@ -41,7 +43,7 @@ if not os.path.exists(context_protein_6Oav["write_dir"]):
 # to generate the protein and ligand files for 6oav
 prot_raw = Protein(context_protein_6Oav["file_path"])
 prot_raw.load(file_path=context_protein_6Oav["file_path"])
-protein, lig = separate_protein_ligand(prot_raw._pdb_file_path)
+protein, lig = separate_protein_ligand(prot_raw.file_path)
 write_mda_universe(protein, context_protein_6Oav["write_dir"] + "/6oav_chA.pdb")
 write_mda_universe(lig, context_protein_6Oav["write_dir"] + "/6oav_A_M3A_lig.pdb")
 protein_obj = Protein(file_path=context_protein_6Oav["write_dir"] + "/6oav_chA.pdb")
@@ -54,12 +56,21 @@ context = GlideContext.get_current()
 
 
 def filter_lines(lines):
+    """Filter out empty lines and strip whitespace."""
     # If no file path to ignore specified, ignore trailing empty or '\n' lines
     filtered_lines = [line.strip() for line in lines if line.strip()]
     return filtered_lines
 
+def extract_m_bond_section(file_content):
+    """Extract the m_bond section from the file content."""
+    start_marker = "m_bond["
+    end_marker = "}"
+    start_index = file_content.find(start_marker)
+    end_index = file_content.find(end_marker, start_index) + len(end_marker)
+    return file_content[start_index:end_index]
 
 def test_convert_protein_to_mae():
+    """Test conversion of protein to MAE format."""
     glide.convert_to_mae(protein_obj, context)
     prot_test_mae = Protein(
         file_path=context_protein_6Oav["write_dir"] + "/6oav_chA.mae"
@@ -70,24 +81,35 @@ def test_convert_protein_to_mae():
 
 
 def test_convert_ligand_to_mae():
+    """Test conversion of ligand to MAE format."""
     glide.convert_to_mae(lig_object, context)
     lig_ref_mae = Ligand(context_protein_6Oav["ligand_file_path"])
     lig_test_mae = Ligand(
-        file_path=context_protein_6Oav["write_dir"] + "/6oav_A_M3A_lig_prepared.mae"
+        file_path=context_protein_6Oav["write_dir"] + "/6oav_A_M3A_lig.mae"
     )
-    lig_ref = filter_lines(lig_ref_mae.ligand_text)
-    lig_test = filter_lines(lig_ref_mae.ligand_text)
-    assert lig_ref == lig_test
 
+    # Extract the m_bond section from both reference and test files
+    lig_ref_content = lig_ref_mae.ligand_text
+    lig_test_content = lig_test_mae.ligand_text
+    lig_ref_m_bond = extract_m_bond_section(lig_ref_content)
+    lig_test_m_bond = extract_m_bond_section(lig_test_content)
 
-def test_prep_Ligand():
+    # Compare the extracted m_bond sections
+    assert lig_ref_m_bond == lig_test_m_bond
+
+def test_prep_ligand():
+    """Test preparation of ligand."""
     prepared = glide.PrepLigand(lig_object, context)
     prep_info = prepared.get_info(context)
     # read the ref and test files and compare the lines
-    with open(context_protein_6Oav["ligand_prepared_path"], "r") as file:
+    with open(
+        context_protein_6Oav["ligand_prepared_path"], "r", encoding="utf-8"
+    ) as file:
         lig_ref = file.readlines()
     with open(
-        context_protein_6Oav["write_dir"] + "/6oav_A_M3A_lig_prepared.mae", "r"
+        context_protein_6Oav["write_dir"] + "/6oav_A_M3A_lig_prepared.mae",
+        "r",
+        encoding="utf-8",
     ) as file:
         lig_test = file.readlines()
     assert lig_ref[113] == lig_test[113]
@@ -98,7 +120,8 @@ def test_prep_Ligand():
     assert prep_info["forcefield"] == "14"
 
 
-def test_prep_Protein():
+def test_prepprotein():
+    """Test preparation of protein."""
     prot_mae = Protein(
         file_path=os.path.join(context_protein_6Oav["write_dir"], "6oav_chA.mae")
     )
@@ -108,7 +131,9 @@ def test_prep_Protein():
     )
     prep_info = prepared.get_info(context)
     # read the grid log file and get the OUTERBOX value and the grid center
-    with open(context_protein_6Oav["grid_log_path"], "r") as file:
+    outerbox_ref = None
+    outerbox_test = None
+    with open(context_protein_6Oav["grid_log_path"], "r", encoding="utf-8") as file:
         lines = file.readlines()
         for line in lines:
             if "OUTERBOX" in line:
@@ -118,9 +143,9 @@ def test_prep_Protein():
                 grid_center_ref = [
                     float(item.strip("[], ")) for item in grid_center_ref
                 ]
-                logger.info(f"The grid center reference numbers are", grid_center_ref)
-
-    with open(context_protein_6Oav["write_dir"] + "/6oav_chA_grid.log", "r") as file:
+                logger.info("The grid center reference numbers are", grid_center_ref)
+    log_file = context_protein_6Oav["write_dir"] + "/6oav_chA_grid.log"
+    with open(log_file, "r", encoding="utf-8") as file:
         lines = file.readlines()
         for line in lines:
             if "OUTERBOX" in line:
@@ -128,7 +153,7 @@ def test_prep_Protein():
             if "GRID_CENTER" in line:
                 temp = [line.split()[1], line.split()[2], line.split()[3]]
                 grid_center_test = [float(item.strip("[], ")) for item in temp]
-                logger.info(f"The grid center test numbers", grid_center_test)
+                logger.info("The grid center test numbers", grid_center_test)
 
     assert np.isclose(float(outerbox_ref), float(outerbox_test), rtol=0.1)
     assert np.all(
@@ -152,6 +177,7 @@ def test_prep_Protein():
 
 
 def test_docking():
+    """Test the docking process."""
     prep_prot = Protein(
         file_path=context_protein_6Oav["write_dir"] + "/6oav_chA_grid.zip"
     )
@@ -159,11 +185,13 @@ def test_docking():
         file_path=context_protein_6Oav["write_dir"] + "/6oav_A_M3A_lig_prepared.mae"
     )
     glide.run(prep_prot, prep_lig, context)
-    with open(context_protein_6Oav["docking_results_path"], "r") as file:
+    with open(
+        context_protein_6Oav["docking_results_path"], "r", encoding="utf-8"
+    ) as file:
         docking_results_ref_reader = csv.DictReader(file)
         docking_results_ref = list(docking_results_ref_reader)
     test_file_path = context_protein_6Oav["write_dir"] + "/6oav_A_M3A_lig_docking.csv"
-    with open(test_file_path, "r") as file:
+    with open(test_file_path, "r", encoding="utf-8") as file:
         docking_results_test_reader = csv.DictReader(file)
         docking_results_test = list(docking_results_test_reader)
     assert np.isclose(
