d72e3b2bba174faaea254e37684327c97d26fc2e
MayarMAhmed <mma121@pitt.edu>
2026-03-13 13:43:57 -0400

    commenting out RMSD until decoupling glide imports

--- lignova/analysis/__init__.py
+++ lignova/analysis/__init__.py
@@ -1,5 +1,8 @@
-r""" Initialization of analysis classes. """
+r"""Initialization of analysis classes."""
 
-from .rmsd import RMSD
+from .gnina_parser import DockedPose, GNINA_Results
 
-__all__ = ["RMSD"]
+# from .rmsd import RMSD
+
+__all__ = ["GNINA_Results", "DockedPose"]
+# __all__ = ["RMSD", "GNINA_Results", "DockedPose"]
