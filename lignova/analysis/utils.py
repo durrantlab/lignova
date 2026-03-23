73fcf9b9abd9ce063c9da05ff7ba29918cb4b877
Mayar Ahmed <mma121@pitt.edu>
2026-03-23 14:22:09 -0400

    removing schrodinger using functions

--- lignova/analysis/utils.py
+++ lignova/analysis/utils.py
@@ -2,86 +2,11 @@
 
 import os
 import subprocess
-from collections.abc import Iterable
 from typing import TextIO
 
 import pandas as pd
 from loguru import logger
 from rdkit.Chem import rdchem, rdmolfiles, rdmolops
-
-from ..docking.contexts import GlideContext
-
-
-def interconvert_mae_sdf(
-    test_file: str | TextIO,
-    output_filename: str,
-    ntruct: int | str | None = None,
-    context: GlideContext = GlideContext.get_current(),
-) -> None:
-    r"""Convert ligand(s) to SDF format.
-
-    Args:
-        test_file : Test file name or file object.
-        output_filename : Output file name.
-        ntruct : Number of structures to convert. Default 1:5 i.e the first 5 structures.
-        context : Docking context to run the command.
-    """
-    # GET THE path of the file extension using the os.path.splitext() function
-    # if the file extension is .sdf, then the file is in SDF format
-    # if the file extension is .mae, then the file is in MAE format
-    # if the file extension is neither .sdf nor .mae, then the file format is not supported
-    if not os.path.exists(test_file):
-        logger.error(f"File {test_file} does not exist.")
-        return
-    logger.info(os.path.splitext(test_file)[1])
-    if os.path.splitext(test_file)[1] == ".sdf":
-        logger.info("Input file is in SDF format.Converting to MAE format.")
-        fileformat = "-isdf"
-        outformat = "-omae"
-    elif (
-        os.path.splitext(test_file)[1] == ".maegz"
-        or os.path.splitext(test_file)[1] == ".mae"
-    ):
-        logger.info("Input file is in MAE format.Converting to SDF format.")
-        fileformat = "-imae"
-        outformat = "-osdf"
-    else:
-        logger.error(
-            "Input file format not supported. Please provide a file in MAE or SDF format."
-        )
-        return
-
-    command = [
-        context.command + "/utilities/sdconvert",
-        fileformat,
-        test_file,
-        outformat,
-        output_filename,
-    ]
-    if ntruct is not None:
-        command.extend(["-n", str(ntruct)])
-    else:
-        command.extend(["-all"])
-    logger.info(f"Running command: {' '.join(command)}")
-    try:
-        with subprocess.Popen(
-            command,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            universal_newlines=True,
-        ) as process:
-            stdout, stderr = process.communicate()
-        if process.returncode == 0:
-            logger.info("File format conversion completed")
-            logger.info(f"Output:\n{stdout}")
-        else:
-            logger.error("File format conversion failed ")
-            logger.error(f"Error Output:\n{stderr}")
-            raise subprocess.CalledProcessError(process.returncode, " ".join(command))
-    except Exception as e:
-        logger.error(f"An error occurred during rmsd calculation: {str(e)}")
-        raise e
-
 
 def obabel_convert(
     test_file: str | TextIO, output_filename: str, hydrogen: bool = False
--- lignova/docking/contexts/__init__.py
+++
@@ -1,7 +0,0 @@
-r"""Initialize the context for the docking and structure class function."""
-
-from .combind import CombindContext
-from .glide import GlideContext
-from .protein import ProteinContext
-
-__all__ = ["GlideContext", "CombindContext", "ProteinContext"]
