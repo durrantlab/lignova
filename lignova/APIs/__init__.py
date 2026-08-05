69eb0e28a8e889af31e777b12511161a035a1eca
MayarMAhmed <mma121@pitt.edu>
2026-08-05 16:15:04 -0400

    feat: updating API init

--- lignova/APIs/__init__.py
+++ lignova/APIs/__init__.py
@@ -3,8 +3,8 @@
 # Source: https://github.com/durrantlab/lignova
 
 """Implementation for API classes."""
-from .pubchem import PubChemAPI
+from .pubchem import PubChemAPI, AssayInfo, CompoundProperties
 from .unichem import UniChemAPI
-from .base import BaseAPI
+from wqm.api import APIResponseType, BaseAPI, ClientConfig, RetryPolicy
 
-__all__ = ["PubChemAPI", "UniChemAPI", "BaseAPI"]
+__all__ = ["PubChemAPI", "UniChemAPI", "BaseAPI", "ClientConfig", "RetryPolicy", "APIResponseType"," AssayInfo", "CompoundInfo", "CompoundSummary", "TargetInfo"]
