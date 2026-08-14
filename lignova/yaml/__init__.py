14dffe9fca3c60ac8b929a5aa1814c30368c7bb0
Mayar Ahmed <mma121@pitt.edu>
2026-08-14 14:23:00 -0400

    feat: adding meeko config base

--- lignova/yaml/__init__.py
+++ lignova/yaml/__init__.py
@@ -8,5 +8,6 @@ from .config import YamlConfig
 from .docking_config import GninaConfig
 from .ligprep_config import GypsumDLConfig
 from .protonation_config import ProtonationConfig
+from .meeko_config import MeekoConfig
 
-__all__ = ["YamlConfig", "ProtonationConfig", "GypsumDLConfig", "GninaConfig"]
+__all__ = ["YamlConfig", "ProtonationConfig", "GypsumDLConfig", "GninaConfig", "MeekoConfig"]
