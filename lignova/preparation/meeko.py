fd1827db7d72d7a931af9e824b23caa07813e90f
Mayar Ahmed <mma121@pitt.edu>
2026-08-19 12:01:54 -0400

    chore: adding license header

--- lignova/preparation/meeko.py
+++ lignova/preparation/meeko.py
@@ -1,3 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2026 University of Pittsburgh — Of the Commonwealth System of Higher Education
+# Source: https://github.com/durrantlab/lignova
+
 r"""Implementation for receptor preparation using Meeko pre-docking."""
 
 import os
